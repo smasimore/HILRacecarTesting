beb863f7882f05602be447a30042ef786c0f161b
smasimore <sarahsmasimore@gmail.com>
2018-04-14 18:25:59 -0500

    [Ping] Beginnings of ping sensor simulator

--- Sensors.c
+++ Sensors.c
@@ -2,8 +2,7 @@
  * File: Sensors.c
  * Author: Sarah Masimore
  * Last Updated Date: 03/14/2018
- * Description: Manages init'ing sensor ports and pins, mapping environment
- * 							output to voltage out on pin to racecar. Uses Ports B and C.
+ * Description: Manages init'ing IR and Ping sensor ports and pins.
  */
 
 #include "Simulator.h"
@@ -14,11 +13,22 @@
 #define NUM_PWM_CHANNELS 6
 #define PWM_PERIOD 8000 // 1000 Hz
 
+#define PING_HOLDOFF_T 60000 // # of clock cycles equal to 750 us (@80MHz)
+
 uint8_t SensorsInitialized = 0;
 
-void initPWMChannel(struct sensor * sensor);
-void setPWMChannelDuty(uint8_t channel, uint16_t duty);
+// Ping sensor globals
+uint32_t CurPingPeriod = 0;
+
+// IR sensor
+void initIRChannel(struct sensor * sensor);
 uint16_t getDutyFromIRSensorVal(uint32_t val);
+void setIRChannelDuty(uint8_t channel, uint16_t duty);
+
+// Ping sensor functions 
+// TODO: Ping untested
+void initPingChannel(struct sensor * sensor);
+uint32_t getPeriodFromPingSensorVal(uint32_t val);
 
 /**
  * Init sensor's required output pins.
@@ -32,12 +42,12 @@ void Sensors_Init(struct car * car) {
 	for (i = 0; i < numSensors; i++) {
 		sensor = &car->sensors[i];
 		switch (sensor->type) {
+			case S_TEST:
 			case S_PING:
-				// todo
+				initPingChannel(sensor);
 				break;
 			case S_IR:
-			case S_TEST:
-				initPWMChannel(sensor);
+				initIRChannel(sensor);
 				break;
 			default:
 				terminal_printString("Error: Unsupported sensor type\r\n");
@@ -62,17 +72,17 @@ void Sensors_Init(struct car * car) {
  * Channel 5 --> PE4 --> M1PWM2
  *
  */
-void initPWMChannel(struct sensor * sensor) {
+void initIRChannel(struct sensor * sensor) {
   volatile unsigned long delay;
-	static uint8_t nextPWMChannel = 0;
+	static uint8_t nextIRChannel = 0;
 	
-	if (nextPWMChannel == NUM_PWM_CHANNELS) {
+	if (nextIRChannel == NUM_PWM_CHANNELS) {
 		terminal_printString("Error: Unsupported number of PWM channels\r\n");
 		return;
 	}
 
 	// Initialize pin based on next available PWM channel.
-	switch (nextPWMChannel) {
+	switch (nextIRChannel) {
 		case 0:
 			// Initialize PWM0 and Port B
 			SYSCTL_RCGCPWM_R |= 0x01;
@@ -209,12 +219,62 @@ void initPWMChannel(struct sensor * sensor) {
 			break;
 		
 		default:
-			terminal_printString("Error: Unsupported number of PWM channels\r\n");
+			terminal_printString("Error: Unsupported number of IR sensors\r\n");
 			return;
 	}
 	
 	// Set sensor's channel and increment.
-	sensor->channel = nextPWMChannel++;
+	sensor->channel = nextIRChannel++;
+}
+
+/**
+ * Currently only supports one Ping sensor on PA2 and Timer0.
+ */ 
+void initPingChannel(struct sensor * sensor) {
+  volatile unsigned long delay;
+	static uint8_t nextIRChannel = 0;
+	
+	if (nextIRChannel > 0) {
+		terminal_printString("Error: Unsupported number of Ping sensors\r\n");
+		return;
+	}
+
+	// Initialize pin to use for output and input signal
+	SYSCTL_RCGCGPIO_R |= 0xC; // Init Port A
+	delay = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTA_AFSEL_R &= ~0xC; // Disable alternate function for PA2
+	GPIO_PORTA_PCTL_R &= ~0xFF00; // Clear PA2 (GPIO)
+	GPIO_PORTA_AMSEL_R &= ~0xC; // Disable analog functionality on PA0=2
+	GPIO_PORTA_DEN_R |= 0xC; // Enable digital I/O on PA2
+ 	GPIO_PORTA_DIR_R &= ~0x4; // Start port 0 as in 
+	GPIO_PORTA_PDR_R |= 0x4; // Pull down register
+
+  // TESTING todo rm
+	GPIO_PORTA_DIR_R |= 0x8;	
+	GPIO_PORTA_DATA_R = 0x8; // PA3 high for testing
+	
+	// Enable interrupt on rising edge
+  GPIO_PORTA_IS_R &= ~0x4; // PA0 is edge-sensitive
+  GPIO_PORTA_IBE_R &= ~0x4; // PA0 is not both edges
+  GPIO_PORTA_IEV_R |= 0x4; // PA0 rising edge event
+  GPIO_PORTA_ICR_R = 0x4; // Clear flag
+  GPIO_PORTA_IM_R |= 0x4; // Arm interrupt
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFFFFFF00)|0x40; // PPriority 2
+  NVIC_EN0_R |= 0x1; // Enable interrupt 0 in NVIC
+	
+	// Initialize timer to interrupt and send Ping response
+	SYSCTL_RCGCTIMER_R |= 0x1; // Activate timer0
+	TIMER0_CTL_R = 0; // Disable until receive pulse from racecar
+	TIMER0_CFG_R = 0; // Configure for 32-bit timer mode (uses A and B)
+	TIMER0_TAMR_R = 0x1; // Configure for one-shot mode, default down-count settings
+	TIMER0_TAILR_R = PING_HOLDOFF_T; // Period
+	TIMER0_TAPR_R = 0; // Prescale value for trigger
+	TIMER0_IMR_R = 1; // Arm interrupt
+	NVIC_EN0_R |= 1<<19; // Enable interrupt 19 in NVIC
+	NVIC_PRI4_R = (NVIC_PRI4_R&(~0xF0000000)) | 0x40000000; // Priority 2
+	TIMER0_ICR_R = 0x1; // Clear flag
+
+	sensor->channel = nextIRChannel++;
 }
 
 /**
@@ -231,16 +291,13 @@ void Sensors_UpdateOutput(struct car * car) {
 	for (i = 0; i < car->numSensors; i++) {
 		sensor = &car->sensors[i];
 		switch (sensor->type) {
-			case S_TEST:
-				duty = 1000 / NUM_PWM_CHANNELS * sensor->channel;
-				setPWMChannelDuty(sensor->channel, duty);
-				break;
 			case S_IR:
 			  duty = getDutyFromIRSensorVal(sensor->val);
-				setPWMChannelDuty(sensor->channel, duty);
+				setIRChannelDuty(sensor->channel, duty);
 				break;
+			case S_TEST:
 			case S_PING:
-				// todo
+				CurPingPeriod = getPeriodFromPingSensorVal(sensor->val);
 				break;
 			default:
 				terminal_printString("Error: Unsupported sensor type\r\n");
@@ -258,10 +315,15 @@ uint16_t getDutyFromIRSensorVal(uint32_t val) {
 	return 500; // 50.0%
 }
 
+uint32_t getPeriodFromPingSensorVal(uint32_t val) {
+	// todo
+	return 8000;
+}
+
 /**
  * Used to test pwm outputs. Duty cycle is % of time low (resolution .1%).
  */
-void setPWMChannelDuty(uint8_t channel, uint16_t duty) {
+void setIRChannelDuty(uint8_t channel, uint16_t duty) {
 	uint32_t duty_ticks;
 	
 	if (duty > 1000) {
@@ -311,4 +373,59 @@ void setPWMChannelDuty(uint8_t channel, uint16_t duty) {
 			terminal_printString("Error: Unsupported PWM channel\r\n");
 			return;
 	}
+}
+
+// TODO: looks like this is triggering off of PA1... is it armed?
+/**
+ * Triggered when get signal from racecar's Ping sensor pin.
+ * Currently only supports PA2.
+ */
+void GPIOPortA_Handler(void){
+	// PA2
+  if(GPIO_PORTA_RIS_R&0x04){
+		// Acknowledge
+    GPIO_PORTA_ICR_R = 0x4;
+		GPIO_PORTA_IM_R |= ~0x4; // Disarm interrupt
+		
+		// Enable Timer0
+		TIMER0_TAILR_R = PING_HOLDOFF_T; 
+		TIMER0_CTL_R = 1;
+  }
+}
+
+/**
+ * This triggers in pairs. The first time, send a high over the Ping pin (PA2).
+ * The second time, stop sending high and set PA2 to input again.
+ */
+void Timer0A_Handler(void){
+	static uint8_t sendHigh = 1;
+	
+	// Acknowledge
+	TIMER0_ICR_R = TIMER_ICR_TATOCINT;
+
+	// Start sending high signal to racecar's Ping input pin
+	if (sendHigh) {
+		// Flip pin to output
+		GPIO_PORTA_DIR_R |= 0x4; // Set as output
+		GPIO_PORTA_DATA_R = 0x4; // Send high signal
+		
+		// Reset timer with period required to represent distance to nearest wall
+		TIMER0_CTL_R = 1;
+		TIMER0_TAILR_R = CurPingPeriod;
+		
+		sendHigh = 0;
+	} else {
+		// Stop sending high
+		GPIO_PORTA_DATA_R &= ~0x4; // Set low
+		GPIO_PORTA_DIR_R &= ~0x4; // Set as input
+		
+		// Arm interrupt
+	  GPIO_PORTA_ICR_R = 0x4; // Clear flag
+		GPIO_PORTA_IM_R |= 0x4; // Arm interrupt
+		
+		// Disable timer
+		TIMER0_CTL_R = 0;
+		
+		sendHigh = 1;
+  }
 }
